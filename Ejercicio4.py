2714764805ea9f32e93f7fb1b1efe7242b801481
Leandro Magallanes <thesharkcommunity@gmail.com>
2023-08-01 22:36:12 -0300

    agregar ejercicio 4 if, elif,else

---
+++ Ejercicio4.py
@@ -0,0 +1,9 @@
+numeroUno = int(input("Ingrese un numero: "))
+numeroDos = int(input("Ingrese otro numero: "))
+
+if numeroUno > numeroDos :
+    print("El numero " + str(numeroUno) + " es mayor que " + str(numeroDos) )
+elif numeroUno < numeroDos : 
+    print("El numero " + str(numeroUno) + " es menor que " + str(numeroDos) ) 
+else : 
+    print("Los números son iguales")
