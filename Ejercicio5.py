22668bd49f5820b82f3b3e33be7981e704242b0c
Leandro Magallanes <thesharkcommunity@gmail.com>
2023-08-04 10:31:46 -0300

    ejercicio 5 operadores AND, OR, NOT

---
+++ Ejercicio5.py
@@ -0,0 +1,24 @@
+a = True
+b = False
+
+print(a and a) # True and True
+
+print(a and b) # True and False
+
+print(a or b) # True or false
+
+print(( 3 < 4 ) and ( 4 < 5 ))
+
+#EL operador AND sirve para reemplazar el operador & en Javascript
+
+# EL operador OR sirve para reemplazar el operador ╣╣ en javascript 
+
+
+
+#Negacion se utiliza el operador NOT para reemplazar a != en javascript
+
+resultado = a and a
+print(not resultado)
+
+resultado = a and a
+print(resultado)
